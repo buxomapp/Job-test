818054e9a072b15786de9325e3776c8f44b5b01b
Ali <moalighaliany@gmail.com>
2024-10-21 23:55:24 +0300

    edit model product

--- server/app/Models/Product.php
+++ server/app/Models/Product.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['shopping_list_id', 'name', 'quantity', 'is_purchased'];
+
+    public function shoppingList()
+    {
+        return $this->belongsTo(ShoppingList::class);
+    }
 }
